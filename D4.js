3fc61a9e34b02dc8dbe922f750ca624facad0616
Vale-n-tina <valentia.grg@gmail.com>
2024-10-10 16:35:13 +0200

    .

--- D4.js
+++ D4.js
@@ -4,6 +4,10 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const area = function (li, l2) {
+  console.log(li * l2);
+};
+area(10, 5);
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
@@ -12,6 +16,12 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const crazysum = function ( number1 , number2) {
+    let sum =(number1+number2)
+};
+
+
+crazysum(5,2)
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
@@ -26,6 +36,14 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const boundary = function (n) {
+  if (n >= 20 || n <= 100 || n === 400) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+console.log(boundary(33));
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -40,6 +58,15 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+const check3and7 = function (num) {
+    if( num%3===0 || num%7===0){
+        return " è accettato"
+      } else 
+      return "non è accettato"
+}
+
+console.log ("il nuemro"+check3and7(21))
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
@@ -60,6 +87,7 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
